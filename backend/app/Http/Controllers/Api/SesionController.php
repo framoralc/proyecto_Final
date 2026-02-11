9e8590a55df10ea7d73ff700dd9fca3605901b0f
framoralc <framoralc@alu.edu.gva.es>
2026-02-11 03:09:05 +0100

    Se han inicializado las funciones del controlador y se han creado los archivos correspondientes

---
+++ backend/app/Http/Controllers/Api/SesionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class SesionController extends Controller
+{
+    public function registrarse(Request $request){
+    
+    }
+
+    public function iniciar(Request $request){
+
+    }
+}
